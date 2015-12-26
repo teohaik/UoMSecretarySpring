6ea2319db9494c2da2be5e4bfcfe82d92e016254
Georgios Digkas <ma-los-sis@hotmail.com>
2015-12-26 18:06:26 +0200

    The associated tasks to TODO comment, completed (Info Issues)

--- src/main/java/gr/uom/UoMSecretarySpring/domain/StudentEnrolledToLessonPK.java
+++ src/main/java/gr/uom/UoMSecretarySpring/domain/StudentEnrolledToLessonPK.java
@@ -59,7 +59,7 @@ public class StudentEnrolledToLessonPK implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        //Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof StudentEnrolledToLessonPK)) {
             return false;
         }
